61a46bae631794adb43b6b8817df33ab02c810ca
minhnem <dtc2154801030092@ictu.edu.vn>
2025-03-04 22:36:36 +0700

    connect to db

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+dotenv.config()
+
+const PORT = process.env.PORT
+const dbURL = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.ywhsg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbURL)
+        console.log('connect to db successfully')
+    } catch (error) {
+        console.log(`can not connect to db ${error}`)
+    }
+}
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`server is starting at http://localhost:${PORT}`)
+    })
+}).catch((error) => {
+    console.log(error)
+})
